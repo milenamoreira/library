d18885c83c63cd89f17b1f22ebb5b3cf80463bb6
Milena Moreira <milena.moreira@cobli.co>
2025-09-30 21:16:00 -0300

    Commit 6: Moving method get_charge from Rental to Book.

--- model.py
+++ model.py
@@ -13,6 +13,20 @@ class Book:
 
     def price_code(self) -> int:
         return self.price_code
+    
+    def get_charge(self, days_rented: int) -> float:
+        amount = 0
+        if self.price_code == Book.REGULAR:
+            amount += 2
+            if days_rented > 2:
+                amount += (days_rented - 2) * 1.5
+        elif self.price_code == Book.NEW_RELEASE:
+            amount += days_rented * 3
+        elif self.price_code == Book.CHILDREN:
+            amount += 1.5
+            if days_rented > 3:
+                amount += (days_rented - 3) * 1.5
+        return amount
 
 class Rental:
     def __init__(self, book: Book, days_rented: int):
@@ -25,25 +39,14 @@ class Rental:
     def days_rented(self) -> int:
         return self.days_rented
     
-    def get_charge(self) -> float:
-        amount = 0
-        if self.book.price_code == Book.REGULAR:
-            amount += 2
-            if self.days_rented > 2:
-                amount += (self.days_rented - 2) * 1.5
-        elif self.book.price_code == Book.NEW_RELEASE:
-            amount += self.days_rented * 3
-        elif self.book.price_code == Book.CHILDREN:
-            amount += 1.5
-            if self.days_rented > 3:
-                amount += (self.days_rented - 3) * 1.5
-        return amount
-    
     def get_frequent_renter_points(self):
         points = 1
         if self.book.price_code == Book.NEW_RELEASE and self.days_rented > 1:
             points += 1
         return points
+    
+    def get_charge(self) -> float:
+        return self.book.get_charge(self.days_rented)
 
 class Client:
 
